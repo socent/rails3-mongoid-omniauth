4165193b5db3b8f64718ca2342b7ab25994808b9
Brad Wilcox <brad_wilcox@me.com>
2013-11-19 18:01:13 -0500

    configure session for omniauth

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,23 @@
 class User
 	attr_protected :provider, :uid, :name, :email
 	
+
 	include Mongoid::Document
 	field :provider, type: String
 	field :uid, type: String
 	field :name, type: String
 	field :email, type: String
+
+
+	def self.create_with_omniauth(auth)
+		create! do |user|
+			user.provider = auth['provider']
+			user.uid = auth['uid']
+			if auth['info']
+				user.name = auth['info']['name'] || ""
+				user.email = auth['info']['email'] || ""
+			end
+		end
+	end
+
 end
